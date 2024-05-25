59a75ec34de65d37512bd4697d245c87e062070c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-25 12:02:20 +0500

    -Hikari Setup (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val postgres_version: String by project
+val hikaricp_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -39,4 +42,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
 }
